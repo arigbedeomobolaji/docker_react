388042c5bff8132636cf9a8e62309c58d5aca870
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-11-22 11:13:20 +0100

    changed app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
+// jshint ignore:start
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
 	render(<App />);
-	const linkElement = screen.getByText(
-		/I was changed on the feature branch/i
-	);
+	const linkElement = screen.getByText(/There was a slight change/i);
 	expect(linkElement).toBeInTheDocument();
 });
